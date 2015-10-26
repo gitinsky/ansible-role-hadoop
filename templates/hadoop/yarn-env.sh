541e07a1a0366b5bd3b12f27a514135e1d9f0ff6
Alex Chistyakov <alexclear@gmail.com>
2015-10-26 16:20:34 +0400

    Use more stadartized way of installing JVM

--- templates/hadoop/yarn-env.sh
+++ templates/hadoop/yarn-env.sh
@@ -2,7 +2,7 @@
 export HADOOP_YARN_USER={{ hadoop_user }}
 
 #export JAVA_HOME=/usr/java/latest
-export JAVA_HOME=/usr/java/jdk1.8.0_40
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
   
 YARN_HEAPSIZE=8000
 export YARN_HEAPSIZE
